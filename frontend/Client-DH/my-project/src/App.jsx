be1279908cc30ed63ad405de53470afe2adcfbb1
JuanRumelfanger <umajuanrumel@gmail.com>
2024-02-06 16:26:36 -0300

    Fix problems

--- frontend/Client-DH/my-project/src/App.jsx
+++ frontend/Client-DH/my-project/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
+          <h1 className="text-3xl font-bold underline">
+     peron
     </h1>
     </>
   )
